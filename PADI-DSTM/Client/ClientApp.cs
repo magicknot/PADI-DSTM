f28bd7df9b83d09ac0dc94863df6eddd109cf087
Alberto Filipe Carvalho <lusoafc@gmail.com>
2014-04-08 21:02:44 +0100

    Client final version

--- PADI-DSTM/Client/ClientApp.cs
+++ PADI-DSTM/Client/ClientApp.cs
@@ -35,20 +35,20 @@ namespace Client {
                     Console.WriteLine("4- Base: testSimpleAbort");
                     Console.WriteLine("5- Base: testSimpleCommit");
                     Console.WriteLine("6- Base: testMultipleReads");
-                    Console.WriteLine("------------------");
+                    Console.WriteLine("---------");
                     Console.WriteLine("Tests with more than one client:");
                     Console.WriteLine(" (you need 3 clients: c1 create the padInts; C2 tries to access them; C3 stops the loop of C1)");
-                    Console.WriteLine("13- YOU MUST DO THIS in c1 BEFORE TEST WITH MORE THAN ONE CLIENT (init)");
-                    Console.WriteLine("7- YOU MUST DO THIS in c3 BEFORE TEST WITH MORE THAN ONE CLIENT (create the loops)");
-                    Console.WriteLine("8- If you want that client1 stops the loop");
-                    Console.WriteLine("12- If you want to confirm the value of stopLoop");
+                    Console.WriteLine("7- YOU MUST DO THIS in c1 BEFORE TEST WITH MORE THAN ONE CLIENT (init)");
+                    Console.WriteLine("8- YOU MUST DO THIS in c3 BEFORE TEST WITH MORE THAN ONE CLIENT (create the loops)");
+                    Console.WriteLine("9- If you want that client1 (stops the loop)");
+                    Console.WriteLine("10- If you want to confirm the value of stopLoop");
                     Console.WriteLine("---------");
                     Console.WriteLine("2- If you want that client1 do a simple read");
                     Console.WriteLine("3- If you want that client1 do a simple write");
                     Console.WriteLine("6- If you want that client1 do multiple read");
-                    Console.WriteLine("9- If you want that client2 do a simple read");
-                    Console.WriteLine("10- If you want that client2 do a simple write");
-                    Console.WriteLine("11- If you want that client2 do a multiple read");
+                    Console.WriteLine("11- If you want that client2 do a simple read");
+                    Console.WriteLine("12- If you want that client2 do a simple write");
+                    Console.WriteLine("13- If you want that client2 do a multiple read");
 
                     input = Console.ReadLine();
 
@@ -76,41 +76,8 @@ namespace Client {
                         client.testMultipleRead(client.getNextUid(), client.getNextUid(), client.getNextUid());
                     }
 
-                    /* loop setup */
-                    if(input.Equals("7")) {
-                        //stopLoop = false;
-                        client.setStopLoop(false);
-                        Console.WriteLine("loop is activated stopLoop = " + client.StopLoop);
-                    }
-
-                    /* stops the loop */
-                    if(input.Equals("8")) {
-                        client.setStopLoop(true);
-                        Console.WriteLine("loop is deactivated stopLoop = " + client.StopLoop);
-                    }
-
-                    /* client2 simple read */
-                    if(input.Equals("9")) {
-                        client.testSimpleReadClient2(client.getNextUid());
-                    }
-
-                    /* client2 simple write */
-                    if(input.Equals("10")) {
-                        client.testSimpleWriteClient2(client.getNextUid());
-                    }
-
-                    /* client2 multiple read */
-                    if(input.Equals("11")) {
-                        client.testMultipleReadClient2(client.getNextUid(), client.getNextUid(), client.getNextUid());
-                    }
-
-                    /* value of stopLoop */
-                    if(input.Equals("12")) {
-                        Console.WriteLine("stopLoop = " + client.StopLoop);
-                    }
-
                     /* init */
-                    if(input.Equals("13")) {
+                    if(input.Equals("7")) {
 
                         try {
                             channel = new TcpChannel(6085);
@@ -123,6 +90,39 @@ namespace Client {
                             Console.WriteLine(e.getMessage());
                         }
                         Console.WriteLine("init done");
+                    }
+
+                    /* loop setup */
+                    if(input.Equals("8")) {
+                        //stopLoop = false;
+                        client.setStopLoop(false);
+                        Console.WriteLine("loop is activated stopLoop = " + client.StopLoop);
+                    }
+
+                    /* stops the loop */
+                    if(input.Equals("9")) {
+                        client.setStopLoop(true);
+                        Console.WriteLine("loop is deactivated stopLoop = " + client.StopLoop);
+                    }
+
+                    /* value of stopLoop */
+                    if(input.Equals("10")) {
+                        Console.WriteLine("stopLoop = " + client.StopLoop);
+                    }
+
+                    /* client2 simple read */
+                    if(input.Equals("11")) {
+                        client.testSimpleReadClient2(client.getNextUid());
+                    }
+
+                    /* client2 simple write */
+                    if(input.Equals("12")) {
+                        client.testSimpleWriteClient2(client.getNextUid());
+                    }
+
+                    /* client2 multiple read */
+                    if(input.Equals("13")) {
+                        client.testMultipleReadClient2(client.getNextUid(), client.getNextUid(), client.getNextUid());
                     }
                 }
 
