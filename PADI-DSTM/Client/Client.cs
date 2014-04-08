589be7e17fb2b5a74d83bcf0509b5df4748c97be
Alberto Filipe Carvalho <lusoafc@gmail.com>
2014-04-08 12:06:48 +0100

    Client: testing with multiple clients part 2

--- PADI-DSTM/Client/Client.cs
+++ PADI-DSTM/Client/Client.cs
@@ -12,6 +12,8 @@ namespace Client {
 
         private static Random random = new Random();
         private static int nextUid = 0;
+        /* if true stops the loop */
+        private static bool stopLoop = true;
 
         public Client() {
             //random = new Random();
@@ -22,6 +24,10 @@ namespace Client {
             /* first arg of Next its the minimum and the second arg is the maximum */
             nextUid = random.Next(0, 100);
             return nextUid;
+        }
+
+        internal bool StopLoop {
+            set { stopLoop = value; }
         }
 
         public static void testRandom() {
@@ -50,6 +56,10 @@ namespace Client {
                 Console.WriteLine("padInt0 created with uid: " + uid0);
 
                 Console.WriteLine("padInt0 read: " + padInt0.read());
+
+                /* to test with more than one client */
+                while(!stopLoop)
+                    ;
 
                 Library.txCommit();
                 Console.WriteLine("txCommit Done");
@@ -81,6 +91,10 @@ namespace Client {
                 if(padInt0.write(20)) {
                     Console.WriteLine("padInt0 write done with value (20) : " + padInt0.read());
                 }
+
+                /* to test with more than one client */
+                while(!stopLoop)
+                    ;
 
                 Library.txCommit();
                 Console.WriteLine("txCommit Done");
@@ -222,6 +236,114 @@ namespace Client {
                     Console.WriteLine("BUG!!!!!: multiple read was not successful...");
                 }
 
+                /* to test with more than one client */
+                while(!stopLoop)
+                    ;
+
+                Library.txCommit();
+                Console.WriteLine("txCommit Done");
+
+                Console.WriteLine("closeChannel Done");
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("------------");
+        }
+
+        //-----------------------------------------------------
+        //client 2
+        public static void testSimpleReadClient2(int uid0) {
+
+            Console.WriteLine("------Test: client2 Simple read ------");
+
+            Library library = new Library();
+
+            Console.WriteLine("library created");
+
+            try {
+                Console.WriteLine("init() Done");
+
+                Library.txBegin();
+                Console.WriteLine("txBegin Done");
+
+                PadIntStub padInt0 = Library.accessPadInt(uid0);
+                Console.WriteLine("padInt0 created with uid: " + uid0);
+
+                Console.WriteLine("padInt0 read: " + padInt0.read());
+
+                Library.txCommit();
+                Console.WriteLine("txCommit Done");
+
+                Console.WriteLine("closeChannel Done");
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("------------");
+        }
+
+        public static void testSimpleWriteClient2(int uid0) {
+
+            Console.WriteLine("------Test: client2 Simple write ------");
+
+            Library library = new Library();
+            Console.WriteLine("library created");
+
+            try {
+                Console.WriteLine("init() Done");
+
+                Library.txBegin();
+                Console.WriteLine("txBegin Done");
+
+                PadIntStub padInt0 = Library.accessPadInt(uid0);
+                Console.WriteLine("padInt0 created with uid: " + uid0);
+
+                if(padInt0.write(20)) {
+                    Console.WriteLine("padInt0 write done with value (20) : " + padInt0.read());
+                }
+
+                Library.txCommit();
+                Console.WriteLine("txCommit Done");
+
+                Console.WriteLine("closeChannel Done");
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("------------");
+        }
+
+        public static void testMultipleReadClient2(int uid0, int uid1, int uid2) {
+
+            Console.WriteLine("------Test: Client2 Multiple read ------");
+
+            Library library = new Library();
+            Console.WriteLine("library created");
+
+            try {
+                Console.WriteLine("init() Done");
+
+                Library.txBegin();
+                Console.WriteLine("txBegin Done");
+
+                PadIntStub padInt0 = Library.accessPadInt(uid0);
+                Console.WriteLine("padInt0 created with uid: " + uid0);
+                PadIntStub padInt1 = Library.accessPadInt(uid1);
+                Console.WriteLine("padInt1 created with uid: " + uid1);
+                PadIntStub padInt2 = Library.accessPadInt(uid2);
+                Console.WriteLine("padInt2 created with uid: " + uid2);
+
+                bool result = padInt0.read() == 0;
+                result = (padInt0.read() == padInt1.read());
+                result = (padInt0.read() == padInt2.read());
+
+                if(result) {
+                    Console.WriteLine("it's OK");
+                } else {
+                    Console.WriteLine("BUG!!!!!: multiple read was not successful...");
+                }
+
                 Library.txCommit();
                 Console.WriteLine("txCommit Done");
 
@@ -242,8 +364,9 @@ namespace Client {
                 string input;
 
                 while(true) {
-
+                    Console.WriteLine("----------------------HELP-------------------------");
                     Console.WriteLine("Please, insert the number of the test that you want to run:");
+                    Console.WriteLine("Tests with just one client");
                     Console.WriteLine("1- testRandom");
                     Console.WriteLine("Tests with one client:");
                     Console.WriteLine("2- Base: testSimpleRead");
@@ -251,6 +374,18 @@ namespace Client {
                     Console.WriteLine("4- Base: testSimpleAbort");
                     Console.WriteLine("5- Base: testSimpleCommit");
                     Console.WriteLine("6- Base: testMultipleReads");
+                    Console.WriteLine("------------------");
+                    Console.WriteLine("Tests with more than one client:");
+                    Console.WriteLine(" (you need 3 clients: c1 create the padInts; C2 tries to access them; C3 stops the loop of C1)");
+                    Console.WriteLine("7- YOU MUST DO THIS BEFORE TEST WITH MORE THAN ONE CLIENT (create the loops)");
+                    Console.WriteLine("8- If you want that client1 release the locks");
+                    Console.WriteLine("---------");
+                    Console.WriteLine("2- If you want that client1 do a simple read");
+                    Console.WriteLine("3- If you want that client1 do a simple write");
+                    Console.WriteLine("6- If you want that client1 do multiple read");
+                    Console.WriteLine("9- If you want that client2 do a simple read");
+                    Console.WriteLine("10- If you want that client2 do a simple write");
+                    Console.WriteLine("11- If you want that client2 do a multiple read");
 
                     input = Console.ReadLine();
 
@@ -276,6 +411,33 @@ namespace Client {
 
                     if(input.Equals("6")) {
                         testMultipleRead(getNextUid(), getNextUid(), getNextUid());
+                    }
+
+                    /* loop setup */
+                    if(input.Equals("7")) {
+                        stopLoop = false;
+                        Console.WriteLine("loop is activated");
+                    }
+
+                    /* stops the loop */
+                    if(input.Equals("8")) {
+                        stopLoop = true;
+                        Console.WriteLine("loop is deactivated");
+                    }
+
+                    /* client2 simple read */
+                    if(input.Equals("9")) {
+                        testSimpleReadClient2(getNextUid());
+                    }
+
+                    /* client2 simple write */
+                    if(input.Equals("10")) {
+                        testSimpleWriteClient2(getNextUid());
+                    }
+
+                    /* client2 multiple read */
+                    if(input.Equals("11")) {
+                        testMultipleReadClient2(getNextUid(), getNextUid(), getNextUid());
                     }
                 }
 
