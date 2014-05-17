9e758175cd5d186211f94f5e11261008acde526e
Alberto Filipe Carvalho <lusoafc@gmail.com>
2014-05-17 13:44:13 +0100

    Updates bat script

--- PADI-DSTM/run_dstm.bat
+++ PADI-DSTM/run_dstm.bat
@@ -1,5 +1,5 @@
-start .\Master-Server\bin\Release\Master-Server.exe
-start .\PadInt-Server\bin\Release\PadIntServer.exe
-start .\PadInt-Server\bin\Release\PadIntServer.exe
-start .\Client\bin\Release\Client.exe
+start .\Master-Server\bin\Debug\Master-Server.exe
+start .\PadInt-Server\bin\Debug\PadIntServer.exe
+start .\PadInt-Server\bin\Debug\PadIntServer.exe
+start .\Client\bin\Debug\Client.exe
 
